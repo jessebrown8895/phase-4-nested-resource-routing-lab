77e1c2f3f18dcce0e7e0ebd1be22642a53357f11
Jesse Brown <Brownjesse8895@yahoo.com>
2022-04-28 17:24:23 -0500

    Nested routes
    This was a bit of a tricky lab working with nested routes and then also knowing how to write conditionals within the controller in order to render the json datat that you want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+  def show
+    item = Item.find(params[:id])
+    if item 
+    render json: item, include: :user
+    else 
+      items = Item.all
+      render json: items
+    end
+  end
 
-  def index
-    items = Item.all
+  def index 
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else 
+      items = Item.all
+    end 
     render json: items, include: :user
   end
 
+  def create 
+    user = find_user
+    if user 
+      item = user.items.create(permit_params)
+      render json: item, status: :created
+    end 
+  end 
+
+  private
+
+  def permit_params 
+    params.permit(:name, :description, :price)
+  end 
+  def find_user 
+    User.find(params[:user_id])
+  end 
+  def render_not_found_response
+    render json: { error: "Item house not found" }, status: :not_found
+  end
+
+  def bad_creation
+    render json: {success: "item not created"}, status: :created
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  # resources :items, only: [:index, :show] do 
+  #   resources :users, only: [:show]
+  # end 
+  resources :users, only: [:index, :show] do 
+    resources :items, only: [:show, :index, :create ]
+  end 
+    resources :items, only: [:index]
+    
 end
